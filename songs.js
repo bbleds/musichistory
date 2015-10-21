d1e014bcad5e3361e763e9e0821361cf602c3185
bbleds <bledsoedesigns@gmail.com>
2015-10-20 21:37:29 -0500

    finished original functionality for version4

--- songs.js
+++ songs.js
@@ -27,15 +27,18 @@ songs.unshift("Levels - by Avivii on the album True");
 songs.push("Fractals - by Seven Lions on the album Days to Come");
 
 //Loop over the array and remove any words or characters that obviously don't belong.
-	
-var targetDiv = document.getElementById("main_right_sidebar");
-for (var i = 0; i < songs.length; i++){
-	songs[i] = songs[i].replace(/[^a-zA-Z" ">-]/g, "");
-	songs[i] = songs[i].replace(/[>]/g, "-");
-	console.log(songs[i]);
-	targetDiv.innerHTML += "<p>"+songs[i]+"</p>";
-	
+
+function outputSongs(){	
+		var targetDiv = document.getElementById("indiv_songs");
+		for (var i = 0; i < songs.length; i++){
+			songs[i] = songs[i].replace(/[^a-zA-Z" ">-]/g, "");
+			songs[i] = songs[i].replace(/[>]/g, "-");
+			console.log(songs[i]);
+			targetDiv.innerHTML += "<p>"+songs[i]+"</p>";
+		}
 }
+
+outputSongs();
 
 //  Now, the entire container for the form should not be displayed when the user first visits your page. The song list with the corresponding form should be visible.
 // When the user clicks on "Add Music", the list view should be hidden, and the music form should show.
@@ -43,9 +46,58 @@ for (var i = 0; i < songs.length; i++){
 // Once the user fills out the song form and clicks the add button, you should collect all values from the input fields, add the song to your array of songs, and update the song list in the DOM.
 
 var addMusic = document.getElementById("add_music");
+var listMusic = document.getElementById("list_music");
+var addBtn = document.getElementById("addSong");
+
 function showAdder(){
 	var inputDiv = document.getElementById("add_wrap");
-	inputDiv.classList.toggle("add_vis");
+	var songdiv = document.getElementById("indiv_songs");
+	inputDiv.classList.remove("add_vis");
+	songdiv.classList.add("add_vis");
 }
 
-add_music.addEventListener("click", showAdder);
+function showList(){
+	var inputDiv = document.getElementById("add_wrap");
+	var songdiv = document.getElementById("indiv_songs");
+	inputDiv.classList.add("add_vis");
+	songdiv.classList.remove("add_vis");
+}
+
+function clearInputs(){
+	document.getElementById("song_name").value = "";
+  	document.getElementById("artist_name").value = "";
+	document.getElementById("album_name").value = "";
+}
+
+/* Once the user fills out the song form and clicks the add button, you should collect 
+   all values from the input fields, add the song to your array of songs, and update the song list in the DOM.*/
+
+  function appendSong(){
+  	var songName = document.getElementById("song_name").value;
+  	var artistName = document.getElementById("artist_name").value;
+  	var albumName  = document.getElementById("album_name").value;
+  	var inputDiv = document.getElementById("add_wrap");
+	var songdiv = document.getElementById("indiv_songs");
+  	if(song_name !== "" && artistName !== "" && albumName !== ""){
+  		songs = [];
+  		songs[songs.length] = songName + " - by "+ artistName +" on the album "+albumName;
+  		outputSongs();
+
+  		clearInputs();
+		inputDiv.classList.add("add_vis");
+		songdiv.classList.remove("add_vis");
+
+  	}
+  }
+
+
+addMusic.addEventListener("click", showAdder);
+listMusic.addEventListener("click", showList);
+addBtn.addEventListener("click", appendSong);
+
+
+
+
+
+
+
