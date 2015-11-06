4e15cb7f1d370ce1999d297d2cbf4b9dc306f04a
bbleds <bledsoedesigns@gmail.com>
2015-11-05 22:55:23 -0600

    adding songs.hbs

--- javascripts/callbackOne.js
+++ javascripts/callbackOne.js
@@ -11,22 +11,27 @@ define(["jquery", "manipulate"], function($, manipulate){
 		//hbs should be handled here
 
 		//below needs to be my reference to handlebars?
-		for(var i =0; i < data.songs.length; i++){
-			console.log("sonnnngggyyy");
-			console.log(data.songs[i]);
-			var indivSong = data.songs[i];
 
-			//push into new array to for when user adds songs after seeing more
-			// firstArrObjects.push(indivSong);
+		require(["hbs!../templates/songs"], function(songTemplate){
+	      $("#indiv_songs").html(songTemplate(data));
+	    });
+
+		// for(var i =0; i < data.songs.length; i++){
+		// 	console.log("sonnnngggyyy");
+		// 	console.log(data.songs[i]);
+		// 	var indivSong = data.songs[i];
+
+		// 	//push into new array to for when user adds songs after seeing more
+		// 	// firstArrObjects.push(indivSong);
 
 
-			$("#indiv_songs").append("<p>"+indivSong.title+" - by "+indivSong.artist+" on the album "+indivSong.album+"<button class='deleteSong btn btn-default'>Delete</button></p>");
+		// 	$("#indiv_songs").append("<p>"+indivSong.title+" - by "+indivSong.artist+" on the album "+indivSong.album+"<button class='deleteSong btn btn-default'>Delete</button></p>");
 
-			//connect each button to event listener
-			$(".deleteSong").click(function(){
-				$(this).parent().remove();
-			});
-		}
+		// 	//connect each button to event listener
+		// 	$(".deleteSong").click(function(){
+		// 		$(this).parent().remove();
+		// 	});
+		// }
 	}
 
 				// Add event listener and connection to function
