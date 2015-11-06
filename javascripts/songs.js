2b0969e0c3d27aab497f647d673f9decddb0a8ad
bbleds <bledsoedesigns@gmail.com>
2015-11-05 22:35:24 -0600

    ready to integrate handlebars plugin

--- javascripts/songs.js
+++ javascripts/songs.js
@@ -1,89 +1,18 @@
 define(["jquery", "manipulate", "populate-songs", "addSongFunctionality", "get-more-songs"], 
 	function($, manipulate, populateSongs, addSongF, getMoreSongs){
 
-//vars are now stored inside manipulate
-		// var firstArrObjects=[];
-		// var secondArrObjects=[];
-		// var moreSongsWereAdded = false;
-	//check if manipulate.js is linked right
-		console.log("Manip is below ");
-		console.log(manipulate.getFirstArray());
-
 
 //Ajax call --> first call will now be in populate-songs.js
-	// $.ajax({
-	// 	url: "songsList.json"
-	// }).done(function(data){
-
-	// 	loopOverMySongObjects(data);
-
-	// 	// Add event listener and connection to function
-	// 	$("#addBtn").click(function(){ createNewObjectAndClear(data) });
-
-	// });
-			
 	populateSongs.getInitialSongs();
 
 	//add event listener for handling adding songs
 	$("#addBtn").click(function(){ addSongF.createNewObjectAndClear() });	
 
+	//Ajax call get-more-songs.json
 	getMoreSongs.getSecondSongs();
 
-	//Ajax call for the extra songs
-	// $.ajax({
-	// 	url: "moreSongs.json"
-	// }).done(function(data){
-	// 	showExtraSongs(data);
-	// });
 
 //functions
-
-	// loop over default songs in json
-	// function loopOverMySongObjects(data){
-	// 	for(var i =0; i < data.songs.length; i++){
-	// 		console.log(data.songs[i]);
-	// 		var indivSong = data.songs[i];
-
-	// 		//push into new array to for when user adds songs after seeing more
-	// 		firstArrObjects.push(indivSong);
-
-	// 		$("#indiv_songs").append("<p>"+indivSong.title+" - by "+indivSong.artist+" on the album "+indivSong.album+"<button class='deleteSong btn btn-default'>Delete</button></p>");
-
-	// 		//connect each button to event listener
-	// 		$(".deleteSong").click(function(){
-	// 			$(this).parent().remove();
-	// 		});
-	// 	}
-	// }
-
-	//	loop over second object list
-	// function showExtraSongs(data){
-	// 	console.log("Second Data is: ", data.MoreSongs);
-	// 	$("#moreSongs").click(function(){
-
-	// 			for(var i =0; i < data.MoreSongs.length; i++){
-	// 		var indivSong = data.MoreSongs[i];
-
-	// 		//push into new array to for when user adds songs after seeing more
-	// 		secondArrObjects.push(indivSong);
-
-	// 		$("#indiv_songs").append("<p>"+indivSong.title+" - by "+indivSong.artist+" on the album "+indivSong.album+"<button class='deleteSong btn btn-default'>Delete</button></p>");
-
-	// 		//connect each button to event listener
-	// 		$(".deleteSong").click(function(){
-	// 			$(this).parent().remove();
-	// 		});
-
-	// 	}
-	// 	$("#moreSongs").prop("disabled", true);
-	// 	moreSongsWereAdded = true;
-
-	// 	});
-	// }
-
-	/*When adding song, Create new object, push it into json array, and output 
-	---> main functionality when adding*/
-
 
 	//Show add song panel 
 	function showAdder(){
@@ -116,9 +45,3 @@ define(["jquery", "manipulate", "populate-songs", "addSongFunctionality", "get-m
 	}
 
 });
-
-
-
-
-
-
